377028033f5f0a840a112439a5b0d3b297342293
durgesh14 <34915741+durgesh14@users.noreply.github.com>
2023-02-25 03:14:40 +0530

    updated gitignore

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.connect(process.env.DB_URI);
-
+// require('dotenv').config()
+mongoose.connect("mongodb://127.0.0.1/issue-tracker");
+//Creating mongoose conncetion
 const db = mongoose.connection;
-
+//on error console the error
 db.on("error", console.error.bind(console, "Error connecting to mongo db"));
-
+//on success display connected message
 db.once("open", function () {
   console.log("Connected to mongo-db");
 });
